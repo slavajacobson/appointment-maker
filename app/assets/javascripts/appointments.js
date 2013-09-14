97a9cfce8d037ade951f24850fa8d42dabfc526c
Slava <you@example.com>
2013-09-14 23:19:22 +0000

    dynamic drop down menu for appointment time selection

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -2,6 +2,9 @@ var monthNames = [ "January", "February", "March", "April", "May", "June", "July
 
 $(document).ready(function() {
 
+    $start_hour = 10;
+    $end_hour = 20;
+
     $('#calendar').fullCalendar({
 
         eventClick:function( event, jsEvent, view ) { 
@@ -38,12 +41,20 @@ $(document).ready(function() {
         selectHelper: false,
         
         dayClick: function( date, allDay, jsEvent, view ) {
+          $make_appointment_current_date = date;
 
+          //Prepare a dropdown menu with Time slots for appointment
+          var selected_time = generate_times_for_appointment($("#appointment_appointment_type").val(), $make_appointment_current_date);
+          
+
+          //set the time and date and open the new appointment window
           var full_date = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
 
           $("#full_date").html(full_date);
 
-          $('#timepicker').timepicker('setTime', ((date.getHours() > 12 ? date.getHours() - 12 : date.getHours()) + ":" + date.getMinutes() + " " + (date.getHours() > 11 ? "PM" : "AM")));
+          $('#appointment_time').val(selected_time).change();
+          //date.getHours() + ":" + date.getMinutes() + (date.getMinutes() < 10 ? '0' : '')
+          //$('#timepicker').timepicker('setTime', ((date.getHours() > 12 ? date.getHours() - 12 : date.getHours()) + ":" + date.getMinutes() + " " + (date.getHours() > 11 ? "PM" : "AM")));
 
           //hide the error message from the modal window
           $(".make_appointment_server_info").hide();
@@ -77,9 +88,9 @@ $(document).ready(function() {
         editable: false,
         defaultView: 'agendaWeek',
         allDaySlot: false,
-        minTime: 10,
+        minTime: $start_hour,
+        maxTime: $end_hour,
         slotMinutes:30,
-        maxTime: 20,
         lazyFetching: false,
         events: '/appointments.json',
         loading: function(isLoading, view) {
@@ -93,6 +104,14 @@ $(document).ready(function() {
 
         },
     
+
+    });
+
+    $("select[name='appointment[appointment_type]']").change(function() {
+      
+      
+
+      generate_times_for_appointment($(this).val(), $make_appointment_current_date);
 
     });
 
@@ -147,7 +166,8 @@ $(document).ready(function() {
       }
       else {
         var date = $(this).find("#full_date").html();
-        time = $(this).find("#timepicker").val();
+        //time = $(this).find("#timepicker").val();
+        time = $(this).find("#appointment_time").val();
         $(this).find("#timepicker").val(date + " " + time);
 
       }
@@ -155,7 +175,7 @@ $(document).ready(function() {
       $.post($(this).attr('action') + ".js", $(this).serialize(), function(data) {});
 
       if (time != null) {
-        $(this).find("#timepicker").val(time);
+        //$(this).find("#timepicker").val(time);
       }
       else {
         $(this).find("#timepicker").val(start_time);
@@ -196,4 +216,104 @@ function show_alert(title, message) {
   $('#info_popup .modal-body p').html(message);
 
   $('#info_popup').modal('show');
+}
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+function equalTime(date1, date2) {
+    return date1.getHours() == date2.getHours() && date1.getMinutes() == date2.getMinutes();
+}
+
+var event_sort_asc = function (event1, event2) {
+  if (event1.start > event2.start) return 1;
+  if (event1.start < event2.start) return -1;
+  return 0;
+};
+
+function console_debug(message) {
+  if (debug)
+    console.log(message);
+}
+
+function generate_times_for_appointment(appt_type, date) {
+  var selected_time = null;
+  var appt_times = [60, 90, 120];
+
+  $("select#appointment_time").html("");
+
+  //appointment length 60 (could be 90 or 120 minutes)
+  var appt_length = typeof appt_type !== 'undefined' ? appt_times[appt_type] : appt_times[0];
+
+  var day_events = [];
+
+  $('#calendar').fullCalendar('clientEvents', function(event) {
+    if(event.start.getDate() == date.getDate()) {
+       day_events.push(event);
+
+    } 
+  });
+
+  //sort appointments
+  day_events.sort(event_sort_asc);
+
+  var skip = false;
+  var $start_hour = 10;
+  var $end_hour = 20;
+
+  cur_time_slot = new Date(date.getFullYear(),date.getMonth(),date.getDate(), $start_hour, 0);
+  end_of_day = new Date(date.getFullYear(),date.getMonth(),date.getDate(), $end_hour, 0);
+  start_of_day = new Date(date.getFullYear(),date.getMonth(),date.getDate(), $start_hour, 0);
+
+  while (cur_time_slot < end_of_day) {
+    skip = false;
+
+    
+    for (k in day_events) {
+
+      //is there a 30 minutes gap before the time slot? (AND there's no room ahead)
+      next_appt_k = (eval(k) + 1 < day_events.length ? eval(k) + 1 : k );
+      if (equalTime(addMinutes(cur_time_slot,-30), day_events[k].end) 
+                && (!equalTime(addMinutes(cur_time_slot,appt_length),day_events[next_appt_k].start) && !equalTime(addMinutes(cur_time_slot,appt_length),end_of_day)))
+        skip = true;
+        
+      //does the day start 30 minutes before the time slot? skip it unless it touches the begining of next appointment
+      else if (equalTime(addMinutes(cur_time_slot,-30),start_of_day)
+                && !equalTime(addMinutes(cur_time_slot,appt_length),day_events[0].start)) 
+        skip = true;
+      
+      //does the the timeslot+appointment end time goes over the limit of working day?
+      else if (addMinutes(cur_time_slot,appt_length) > end_of_day) 
+        skip = true;
+       
+      //does the event overlap with the timeslot?
+      else if (day_events[k].start < addMinutes(cur_time_slot, appt_length) && day_events[k].end > cur_time_slot) 
+        skip = true;
+        
+    }
+    if (!skip) {
+      time_formatted = cur_time_slot.getHours() + ":" + cur_time_slot.getMinutes() + (cur_time_slot.getMinutes() < 10 ? '0' : '');
+      $("select#appointment_time").append("<option value='" + time_formatted + "'>" + time_formatted + "</option>");
+
+      if ((cur_time_slot.getHours() == date.getHours() && cur_time_slot.getMinutes() == date.getMinutes()) || cur_time_slot.getHours() == date.getHours())
+        selected_time = cur_time_slot;
+
+    }
+
+    //increase by 30 minutes
+    cur_time_slot = addMinutes(cur_time_slot, 30);
+  }
+
+
+  var formatted_time;
+
+  if (selected_time != null)
+    formatted_time = selected_time.getHours() + ":" + selected_time.getMinutes() + (selected_time.getMinutes() < 10 ? '0' : '');
+  else
+    formatted_time = "";
+
+  return formatted_time;
+
+
 }
